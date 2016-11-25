7f87b26a6bb27c97354ab8dfe825dbc321cc881b
Arunabh98 <arunabhghosh98@gmail.com>
2016-11-25 19:40:19 +0000

    complete until step 7

--- app/src/main/java/com/example/darknight/mi2016/LoginActivity.java
+++ app/src/main/java/com/example/darknight/mi2016/LoginActivity.java
@@ -1,7 +1,7 @@
 package com.example.darknight.mi2016;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
