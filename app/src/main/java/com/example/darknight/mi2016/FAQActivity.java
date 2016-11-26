3b62301dbea99ed53d70bc23795d003dc193f510
TheFlash98 <mastermind.sarthak@gmail.com>
2016-11-26 23:55:09 +0530

    Link xml to Java file

--- app/src/main/java/com/example/darknight/mi2016/FAQActivity.java
+++ app/src/main/java/com/example/darknight/mi2016/FAQActivity.java
@@ -1,8 +1,16 @@
 package com.example.darknight.mi2016;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 /**
  * Created by Sarthak on 26/11/2016.
  */
 
-public class FaqActivity {
+public class FaqActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+    }
 }
