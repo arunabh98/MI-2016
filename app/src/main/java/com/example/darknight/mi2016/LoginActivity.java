7ddd1de67cb4e941351f52f8ad63a4859f3fb0ae
Arunabh98 <arunabhghosh98@gmail.com>
2016-11-26 03:06:14 +0000

    extract username and email id from facebook

--- app/src/main/java/com/example/darknight/mi2016/LoginActivity.java
+++ app/src/main/java/com/example/darknight/mi2016/LoginActivity.java
@@ -3,14 +3,20 @@ package com.example.darknight.mi2016;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
     private LoginButton loginButton;
@@ -30,8 +36,25 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
-                startActivity(intent);
+                GraphRequest request = GraphRequest.newMeRequest(
+                        AccessToken.getCurrentAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject jsonObject, GraphResponse response) {
+                                try {
+                                    Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                                    intent.putExtra("Name", jsonObject.getString("name"));
+                                    intent.putExtra("Email ID", jsonObject.getString("email"));
+                                    startActivity(intent);
+                                } catch (Exception e) {
+                                    Log.e("LoginActivity", "Error in parsing JSON");
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,link,cover,email");
+                request.setParameters(parameters);
+                request.executeAsync();
             }
 
             @Override
--- app/src/main/java/com/example/darknight/mi2016/RegistrationActivity.java
+++ app/src/main/java/com/example/darknight/mi2016/RegistrationActivity.java
@@ -2,6 +2,7 @@ package com.example.darknight.mi2016;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,12 +16,16 @@ public class RegistrationActivity extends AppCompatActivity {
         final EditText field = (EditText) findViewById(R.id.userInputField);
         final Button button = (Button) findViewById(R.id.submit);
         final String[] fieldNames = {"Name", "Email ID", "Mobile No.", "City", "College", "Year of study"};
-        field.setHint(fieldNames[index]);
+        field.setText(getIntent().getStringExtra("Name"));
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Perform action on click
+                if (fieldNames[index].equals("Name") || fieldNames[index].equals("Email ID")) {
+                    Log.w("fdsdf", fieldNames[index]);
+                    field.setText(getIntent().getStringExtra(fieldNames[index]));
+                } else {
+                    field.setText(fieldNames[index]);
+                }
                 index = index + 1;
-                field.setHint(fieldNames[index]);
             }
         });
     }
