b79f479dabf3a0f195accac55e7db65258ffd0a9
TheFlash98 <mastermind.sarthak@gmail.com>
2016-11-27 16:44:38 +0530

    fixed bugs

--- app/src/main/java/com/example/darknight/mi2016/FAQActivity.java
+++ app/src/main/java/com/example/darknight/mi2016/FAQActivity.java
@@ -3,11 +3,8 @@ package com.example.darknight.mi2016;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-/**
- * Created by Sarthak on 26/11/2016.
- */
+public class FAQActivity extends AppCompatActivity {
 
-public class FaqActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
