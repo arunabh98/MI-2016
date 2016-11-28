ba35f1dffbc1f96dcb0152d48326f105c6ee6823
Arunabh98 <arunabhghosh98@gmail.com>
2016-11-28 15:25:55 +0000

    start database connection

--- app/src/main/java/com/example/darknight/mi2016/RegistrationActivity.java
+++ app/src/main/java/com/example/darknight/mi2016/RegistrationActivity.java
@@ -1,10 +1,17 @@
 package com.example.darknight.mi2016;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class RegistrationActivity extends AppCompatActivity {
     int index = 0;
@@ -17,12 +24,14 @@ public class RegistrationActivity extends AppCompatActivity {
         final Button button = (Button) findViewById(R.id.submit);
         final String[] fieldNames = {"Name", "Email ID", "Mobile No.", "City", "College", "Year of study"};
         field.setText(getIntent().getStringExtra("Name"));
-        final String[] userDetails;
+        final String[] userDetails = new String[6];
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                userDetails[index] = field.getText().toString();
                 index = index + 1;
                 if (index >= 6) {
-                    ;
+                    FetchMiNumberTask miNumberTask = new FetchMiNumberTask();
+                    miNumberTask.execute();
                 } else {
                     if (fieldNames[index].equals("Name") || fieldNames[index].equals("Email ID")) {
                         field.setText(getIntent().getStringExtra(fieldNames[index]));
@@ -33,5 +42,44 @@ public class RegistrationActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+}
+
+class FetchMiNumberTask extends AsyncTask<String, Void, Void> {
+    private Exception exception;
+
+    protected Void doInBackground(String... urls) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mi_2016", "root", "pratik123");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from register");
+            Log.e("sdasdas", "dasdasdad");
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
+
+class MysqlCon {
+    public static void main(String args[]) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mi_2016", "root", "pratik123");
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("select * from register");
+
+            while (rs.next())
+                Log.w("sdfsdf", rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+
+            con.close();
+
+        } catch (Exception e) {
+            Log.e("kjhjk", "kjjkj");
+        }
+
     }
 }
