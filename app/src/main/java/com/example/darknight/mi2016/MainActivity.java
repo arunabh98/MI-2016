48d77f77da8f609cce7cf0296ee77dcf228a76bb
Sajal Narang <sajalnarang@gmail.com>
2016-11-28 16:20:09 +0530

    Reformatted code

--- app/src/main/java/com/example/darknight/mi2016/MainActivity.java
+++ app/src/main/java/com/example/darknight/mi2016/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.darknight.mi2016;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
