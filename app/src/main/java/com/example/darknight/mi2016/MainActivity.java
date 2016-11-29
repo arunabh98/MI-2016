de76befc49e1d3ae835552ef09efa3f117050315
Sajal Narang <sajalnarang@gmail.com>
2016-11-30 02:38:24 +0530

    Closed #4

--- app/src/main/java/com/example/darknight/mi2016/MainActivity.java
+++ app/src/main/java/com/example/darknight/mi2016/MainActivity.java
@@ -64,20 +64,21 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_events) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_hospitality) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_going) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_schedule) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_faq) {
+
+        } else if (id == R.id.nav_contact) {
+
+        } else if (id == R.id.nav_map) {
 
         }
 
