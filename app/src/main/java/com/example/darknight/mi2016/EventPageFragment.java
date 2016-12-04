d4974b2712da2b84bd6c753c4790c3543f5e0474
TheFlash98 <mastermind.sarthak@gmail.com>
2016-12-04 23:47:33 +0530

    Change View Names

--- app/src/main/java/com/example/darknight/mi2016/EventPageFragment.java
+++ app/src/main/java/com/example/darknight/mi2016/EventPageFragment.java
@@ -28,17 +28,17 @@ public class EventPageFragment extends Fragment {
 
 
         final View rootView = inflater.inflate(R.layout.fragment_event_page, container, false);
-        ImageButton ib = (ImageButton) rootView.findViewById(R.id.myIconButton);
-        ib.setOnClickListener(new View.OnClickListener() {
+        final ImageButton imageButton = (ImageButton) rootView.findViewById(R.id.myIconButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TextView textView = (TextView) rootView.findViewById(R.id.myTextView1);
-                TextView textView2 = (TextView) rootView.findViewById(R.id.myTextView2);
-                ImageView imageView = (ImageView) rootView.findViewById(R.id.myClockIcon);
-                ImageView imageView2 = (ImageView) rootView.findViewById(R.id.myLocationIcon);
+                TextView textView = (TextView) rootView.findViewById(R.id.Event_venue);
+                TextView textView2 = (TextView) rootView.findViewById(R.id.Time);
+                ImageView imageView = (ImageView) rootView.findViewById(R.id.Clock_icon);
+                ImageView imageView2 = (ImageView) rootView.findViewById(R.id.Location_icon);
                 if(textView.getCurrentTextColor()==getResources().getColor(R.color.yellow))
                 {
-                    ib.setImageResource(R.drawable.ic_notifications_none_white_24px);
+                    imageButton.setImageResource(R.drawable.ic_notifications_none_white_24px);
                     textView.setTextColor(getResources().getColor(R.color.white));
                     textView2.setTextColor(getResources().getColor(R.color.white));
                     imageView.setImageResource(R.drawable.ic_access_time_white_24px);
@@ -50,7 +50,7 @@ public class EventPageFragment extends Fragment {
                     textView2.setTextColor(getResources().getColor(R.color.yellow));
                     imageView.setImageResource(R.drawable.ic_access_time_yellow_24px);
                     imageView2.setImageResource(R.drawable.ic_place_yellow_24px);
-                    ib.setImageResource(R.drawable.ic_notifications_white_24px);
+                    imageButton.setImageResource(R.drawable.ic_notifications_white_24px);
                 }
 
             }
