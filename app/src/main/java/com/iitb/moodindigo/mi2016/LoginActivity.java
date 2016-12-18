dba10e671ca009e9abb7a495fd447d8997172610
Arunabh98 <arunabhghosh98@gmail.com>
2016-12-18 15:54:34 +0000

    modify splash screen

--- app/src/main/java/com/iitb/moodindigo/mi2016/LoginActivity.java
+++ app/src/main/java/com/iitb/moodindigo/mi2016/LoginActivity.java
@@ -80,6 +80,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
