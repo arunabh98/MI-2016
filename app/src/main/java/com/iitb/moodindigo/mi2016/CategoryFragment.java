c48170ef808b0102aec380bc24b3403a2bd6376b
Arunabh98 <arunabhghosh98@gmail.com>
2016-12-18 18:24:44 +0000

    going added to back button

--- app/src/main/java/com/iitb/moodindigo/mi2016/CategoryFragment.java
+++ app/src/main/java/com/iitb/moodindigo/mi2016/CategoryFragment.java
@@ -46,7 +46,7 @@ public class CategoryFragment extends Fragment {
                 Fragment eventPageFragment = new EventPageFragment(getContext(), eventResponse.get(position));
                 FragmentManager manager = getActivity().getSupportFragmentManager();
                 FragmentTransaction transaction = manager.beginTransaction();
-                transaction.addToBackStack(null);
+                transaction.addToBackStack("category");
                 transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left);
                 transaction.replace(R.id.relativelayout_for_fragment, eventPageFragment, eventPageFragment.getTag());
                 transaction.commit();
